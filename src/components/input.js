d4327b915ea15a5facb8960228c69ee24cec9e44
AdrianaOrellan <149824089+AdrianaOrellan@users.noreply.github.com>
2024-05-30 15:39:01 -0600

    Cambios

---
+++ src/components/input.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TextInput, StyleSheet, Dimensions } from 'react-native';
+
+
+// Obtener el ancho de la pantalla
+const { width } = Dimensions.get('window');
+
+const input = ({ placeholder, nombre, setNombre }) => {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      placeholderTextColor="#888"
+      value={nombre}
+      onChangeText={setNombre}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: width - 40,
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    fontSize: 16,
+    backgroundColor: '#f5f5f5',
+    borderWidth: 0,
+  },
+});
+
+export default input;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Alert } from 'react-native';
+import CustomButton from '../components/customButton';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Input from '../components/input';
 
 const { width } = Dimensions.get('window');
 
 const Home = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Este es el home</Text>
-    </View>
-  );
+
+    const [search, setSearch] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Essenzial</Text>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert('Button with adjusted color pressed')}>
+                <MaterialCommunityIcons name="cart" size={18} color="white" />
+            </TouchableOpacity>
+            <Input
+                placeholder="Buscar"
+                nombre={search}
+                setNombre={setSearch}
+            />
+            <Text style={styles.title2}>Marcas favoritas</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    paddingHorizontal: 20,
-  },
-  title: {
-    marginTop: 95,
-    alignSelf: 'flex-start',
-    marginLeft: 10,
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#000',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+        paddingTop: 20,
+    },
+    title: {
+        marginTop: 40,
+        alignSelf: 'flex-start',
+        marginLeft: 10,
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#000',
+    },
+    button: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        backgroundColor: '#000000',
+        borderRadius: 20,
+        padding: 12,
+        marginTop: 40,
+    },
+    title2: {
+        marginTop: 10,
+        alignSelf: 'flex-start',
+        marginLeft: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#000',
+    },
 });
 
 export default Home;
