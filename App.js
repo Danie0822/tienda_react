ce8b64babbbe7b706fe8755fa00554720c2234fa
VRMOTA <w0lf3ryxd@gmail.com>
2024-07-06 17:35:40 -0600

    Splashh

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Animated, Image } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import LoginScreen from './src/screens/login';
 import RecuperacionScreen from './src/screens/recuperacion'; 
 import Registrar from './src/screens/registrar'; 
@@ -11,28 +12,76 @@ import RecuCodigoScreen from './src/screens/recuCodigo';
 import Navigation from './src/navigation/navigation'; 
 import DetalleProducto from './src/screens/detalleProducto';
 
-
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const fadeAnim = useRef(new Animated.Value(1)).current; // Usamos useRef para mantener el valor animado
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        // Simulamos una carga de recursos (ejemplo con setTimeout)
+        setTimeout(() => {
+          setAppIsReady(true);
+        }, 3000); // Tiempo de carga simulado de 3 segundos (ajusta según sea necesario)
+
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (appIsReady) {
+      Animated.timing(
+        fadeAnim,
+        {
+          toValue: 0,
+          duration: 1000,
+          useNativeDriver: true,
+        }
+      ).start(() => {
+        setAnimationComplete(true);
+      });
+    }
+  }, [appIsReady, fadeAnim]);
+
+  if (!animationComplete) {
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.splashContainer, { opacity: fadeAnim }]}>
+          <Image
+            source={require('./src/img/Logoe.png')}
+            style={styles.logo}
+          />
+        </Animated.View>
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false
-          }}
-        >
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Recuperacion" component={RecuperacionScreen} />
-          <Stack.Screen name="RecuperacionCodigo" component={RecuCodigoScreen} />
-          <Stack.Screen name="Home" component={Navigation}/>
-          <Stack.Screen name="RecuContra" component={RecuContra}/>
-          <Stack.Screen name="Registrar" component={Registrar}/>
-          <Stack.Screen name="DetalleProducto" component={DetalleProducto}/>
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Recuperacion" component={RecuperacionScreen} />
+        <Stack.Screen name="RecuperacionCodigo" component={RecuCodigoScreen} />
+        <Stack.Screen name="Home" component={Navigation}/>
+        <Stack.Screen name="RecuContra" component={RecuContra}/>
+        <Stack.Screen name="Registrar" component={Registrar}/>
+        <Stack.Screen name="DetalleProducto" component={DetalleProducto}/>
+      </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
@@ -41,5 +90,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splashContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#000000', // Mismo color de fondo que la pantalla de inicio
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200, // Ancho deseado
+    height: 200, // Alto deseado
   },
 });
