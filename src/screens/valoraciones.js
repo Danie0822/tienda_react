3f683eb16b58dfb6ccc2179b319d8c4b55520028
AdrianaOrellan <adrianaorellana68@gmail.com>
2024-07-19 18:09:49 -0600

    Cambio de nombre en js

--- src/screens/valoraciones.js
+++ src/screens/valoraciones.js
@@ -4,7 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Rating } from 'react-native-ratings';
 import CustomFlecha from '../components/regresar';
-import useValoraciones from '../controller/publica/valoraciones'; 
+import useValoraciones from '../controller/publica/valoraciones_inventario'; 
 
 // Función para formatear la fecha
 const formatDate = (date) => {
